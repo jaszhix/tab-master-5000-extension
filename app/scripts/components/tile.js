a5062077171caadf3fee06f8fd25dcc57d4cb31f
Jason Hicks <jaszhix@gmail.com>
2016-02-16 11:03:57 -0600

    Remove unnecessary state setting

--- app/scripts/components/tile.js
+++ app/scripts/components/tile.js
@@ -187,14 +187,12 @@ var Tile = React.createClass({
       for (var i = t.length - 1; i >= 0; i--) {
         if (t[i].id !== first.id && t[i].title !== 'New Tab' && t[i].id !== activeTab) {
           if (opt === 'close') {
-            this.setState({duplicate: false});
             this.handleCloseTab(t[i].id);
           } else if (s.tab.id === t[i].id && s.stores.prefs.duplicate) {
             this.handleFocus('duplicate',true,p);
           }
         }
       }
-      
     }
   },
   closeNewTabs(){
