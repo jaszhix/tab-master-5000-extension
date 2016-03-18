2b4cb23f35a2c015a9069e73406bd32b3e5374f4
Jason Hicks <jaszhix@gmail.com>
2016-03-17 23:27:55 -0500

    Move screenshot capturing to content script with html2canvas

--- app/scripts/content/content.js
+++ app/scripts/content/content.js
@@ -1,4 +1,28 @@
 import kmp from 'kmp';
+import html2canvas from 'html2canvas';
+
+var getImage = (type)=>{
+  chrome.runtime.sendMessage(chrome.runtime.id, {type: 'checkSSCapture'}, (response)=>{
+    console.log(response)
+    if (response.length === 0 || type === 'activate') {
+      html2canvas(document.body,{
+        height: window.innerHeight,
+        width: window.innerWidth,
+        allowTaint: false,
+        useCORS: true,
+        background: '#FFFFFF',
+        onrendered: function(canvas) {
+          var quality =  0.2;
+          var dataUrl = canvas.toDataURL('image/jpeg', quality);
+          console.log(dataUrl);
+          chrome.runtime.sendMessage(chrome.runtime.id, {type: 'screenshot',image: dataUrl}, (response)=>{});
+        }
+      }).catch(()=>{
+        chrome.runtime.sendMessage(chrome.runtime.id, {type: 'screenshot',image: false}, (response)=>{});
+      });
+    }
+  });
+};
 
 var getBlacklist = new Promise((resolve, reject)=>{
   chrome.storage.sync.get('blacklist', (bl)=>{
@@ -7,23 +31,32 @@ var getBlacklist = new Promise((resolve, reject)=>{
     }
   });
 });
-getBlacklist.then((bl)=>{
-  var getPrefs = new Promise((resolve, reject)=>{
-    chrome.storage.sync.get('preferences', (prefs)=>{
-      if (prefs && prefs.preferences) {
-        resolve(prefs);
+var getPrefs = new Promise((resolve, reject)=>{
+  chrome.storage.sync.get('preferences', (prefs)=>{
+    if (prefs && prefs.preferences) {
+      resolve(prefs);
+    }
+  });
+});
+getPrefs.then((prefs)=>{
+  var blacklistPref = prefs.preferences.blacklist;
+  var screenshotsPref = prefs.preferences.screenshot;
+  if (screenshotsPref) {
+    getImage(false);
+    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+      if (msg.type === 'activate') {
+        getImage(msg.type);
       }
     });
-  });
-  getPrefs.then((prefs)=>{
-    var blacklistPref = prefs.preferences.blacklist;
-    if (blacklistPref) {
+  }
+  if (blacklistPref) {
+    getBlacklist.then((bl)=>{
       for (var i = 0; i < bl.blacklist.length; i++) {
         if (kmp(window.location.href, bl.blacklist[i]) !== -1) {
           chrome.runtime.sendMessage({method: 'close'}, (response)=>{
           });
         }
       }
-    }
-  });
+    });
+  }
 });
